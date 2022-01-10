83e46a4a15a2a9f56dd09409616da8b78d917267
Dekopon06 <92018465+Dekopon06@users.noreply.github.com>
2022-01-10 17:53:06 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,18 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :basic_auth, :configure_permitted_parameters, if: :devise_controller?
 
-  private
+
+ private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(
       :sign_up, keys: [:nickname, :email, :encrypted_password, :sex_id, :age_id, :job_id]
     )
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'admin' && password == '2222'
+    end
   end
   
 end
